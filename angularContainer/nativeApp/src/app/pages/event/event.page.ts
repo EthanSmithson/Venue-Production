1d56df354f76219b778bbe99e7f6695e6d997d40
root <ethansmitson413@gmail.com>
2024-11-12 20:33:50 -0500

    Added google maps api for location of venue

--- angularContainer/nativeApp/src/app/pages/event/event.page.ts
+++ angularContainer/nativeApp/src/app/pages/event/event.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonBackButton, IonHeader, IonTitle, IonToolbar, IonButtons, IonImg, IonItem, IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonIcon } from '@ionic/angular/standalone';
@@ -7,15 +7,20 @@ import { inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { heart } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { GoogleMapsModule } from "@angular/google-maps";
+import { Loader } from "@googlemaps/js-api-loader"
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.page.html',
   styleUrls: ['./event.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonImg, IonItem, IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonIcon ]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonImg, IonItem, IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle, IonIcon, GoogleMapsModule ]
 })
-export class EventPage implements OnInit {
+export class EventPage implements AfterViewInit {
+
+  @ViewChild('mapEl') mapEl!: ElementRef;
 
   private EventsDetails = inject(EventDetailsService);
   eventDetails: any
@@ -24,10 +29,16 @@ export class EventPage implements OnInit {
   eventStartDate: String;
   eventStartTime: String;
   eventTickets: String;
+  LatLng: any;
   
   constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+
+    let loader = new Loader({
+      apiKey: 'AIzaSyD4AJX7dsVR4DkYHUP-J3exqx9c4Q4ucL8',
+      version: "weekly"
+    })
 
     addIcons({ heart })
     
@@ -38,7 +49,111 @@ export class EventPage implements OnInit {
       this.eventName = this.eventDetails.name;
       this.eventImages = this.eventDetails.images[0].url;
       this.eventTickets = this.eventDetails.url;
-    });
+      console.log(this.eventDetails._embedded.venues[0].location)
+      this.LatLng = { "lat": Number(this.eventDetails._embedded.venues[0].location.latitude), "lng" : Number(this.eventDetails._embedded.venues[0].location.longitude) }
+      console.log(this.LatLng)
+
+      loader
+        .importLibrary('maps')
+        .then(({Map}) => {
+          new Map(this.mapEl.nativeElement, {
+            center: this.LatLng,
+            zoom: 13, 
+            styles: [
+              {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "labels.text",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi.business",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "road",
+                "elementType": "labels.icon",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.arterial",
+                "elementType": "labels",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "stylers": [
+                  {
+                    "visibility": "simplified"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "labels",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.local",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.local",
+                "elementType": "labels",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              }
+            ]
+            // mapId: "72dbc0caa69649c6"
+          });
+        })
+        .catch((e) => {
+          // do something
+      });
+      });
 
   }
 
